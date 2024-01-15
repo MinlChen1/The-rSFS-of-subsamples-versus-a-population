d9113eb65cfd4dd0a68d7709603f18a35d31a590
MinlChen1 <153777990+MinlChen1@users.noreply.github.com>
2024-01-15 22:18:33 +0100

    Update 2.run_Rscript.sh

--- Updated Scripts/2.run_Rscript.sh
+++ Updated Scripts/2.run_Rscript.sh
@@ -1,5 +1,5 @@
 #run R script
-NITER=10
+NITER=100
 PATHR=`realpath`
 R --vanilla --args 1 100 $NITER $PATHR < ./Sweep_SFSline_diversity_sims.R #calculation for haplotypes
 R --vanilla --args 2 100 $NITER $PATHR < ./Sweep_SFSline_diversity_sims.R #calculation for diploid individuals
